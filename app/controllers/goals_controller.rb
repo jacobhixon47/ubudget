8782923d037901563147d539385238d97ad5b74f
Jacob Hixon <jacobhixon47@gmail.com>
2017-03-06 13:10:47 -0800

    allow users to update category.goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -9,6 +9,8 @@ class GoalsController < ApplicationController
       redirect_to home_index
     end
   end
+
+# Start super long Create logic
   def create
     if user_signed_in?
       @user = current_user
@@ -26,11 +28,13 @@ class GoalsController < ApplicationController
       # If no monthly/weekly goal has been set so far
       else
         if @goal.save
+          # If goal is weekly, add all transaction amounts for current week to spent_amount
           if @goal.weekly?
             @category.transactions.where(:date => Date.today.beginning_of_week..Date.today.end_of_week).each do |transaction|
               @goal.spent_amount += transaction.amount
               @goal.save
             end
+          # If goal is monthly, add all transaction amounts for current month to spent_amount
           else
             @category.transactions.where(:date => Date.today.beginning_of_month..Date.today.end_of_month).each do |transaction|
               @goal.spent_amount += transaction.amount
@@ -49,7 +53,36 @@ class GoalsController < ApplicationController
       redirect_to home_index
     end
   end
+# End Create logic
 
+  def edit
+    if user_signed_in?
+      @user = current_user
+      @category = @user.categories.find(params[:category_id])
+      @goal = @category.goals.find(params[:id])
+    else
+      flash[:notice] = "You must be signed in to access this feature"
+      redirect_to home_index
+    end
+  end
+
+  def update
+    if user_signed_in?
+      @user = current_user
+      @category = @user.categories.find(params[:category_id])
+      @goal = @category.goals.find(params[:id])
+      if @goal.update(goal_params)
+        flash[:notice] = "Your goal was updated successfully!"
+        redirect_to user_transactions_path(@user)
+      else
+        flash[:alert] = "Something went wrong. Please try again."
+        render :edit
+      end
+    else
+      flash[:notice] = "You must be signed in to access this feature"
+      redirect_to home_index
+    end
+  end
 private
   def goal_params
     params.require(:goal).permit(:base_amount, :category_id, :weekly)
