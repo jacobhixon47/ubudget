dc0c8c0b01b2a46200d0506cb384582116886f75
Jacob Hixon <jacobhixon47@gmail.com>
2017-04-09 16:06:38 -0700

    add confirmation for email accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   def after_database_authentication
     Goal.update_spending
